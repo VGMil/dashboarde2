b510ac986ab01a621c36ebe784b1dd6923005891
miltonvg <miltonvg@uets.edu.ec>
2025-09-17 17:37:36 +0000

    💄ui(ProfileDetails): Crear los detalles principales del usuario. - Añadir imagen de perfil. - Añadir nombre y profesion del usuario.

--- components/ProfileDetails.tsx
+++ components/ProfileDetails.tsx
@@ -1,10 +1,18 @@
-import { View, Text } from 'react-native'
+import "@/global.css"
 import React from 'react'
-
+import { Image, Text, View } from 'react-native'
 const ProfileDetails = () => {
   return (
-    <View>
-      <Text>ProfileDetails</Text>
+    <View className="flex justify-center items-center">
+      <Image 
+        source={require('../assets/images/icon.png')} 
+        className="max-h-36 max-w-36 rounded-full"
+      />
+      <Text className="text-2xl font-bold">Milton Velasquez</Text>
+      <Text className="text-xl font-bold text-gray-400">Profesor</Text>
+
+
+      
     </View>
   )
 }
