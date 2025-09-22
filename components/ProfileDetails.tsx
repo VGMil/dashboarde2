39e820ecfe950e7017d0d9a2dc8694a57b8b6757
miltonvg <miltonvg@uets.edu.ec>
2025-09-22 21:37:13 +0000

    ✨feat(IconButton): Crear un IconButton Component con props - Uso de pressable y expo-icons para crear un boton. - Añadir props color:string, iconName:string, pulsar:()=>void, enlace:string - (ProfileDetails) añadir 4 IconButton

--- components/ProfileDetails.tsx
+++ components/ProfileDetails.tsx
@@ -1,18 +1,24 @@
 import "@/global.css"
 import React from 'react'
 import { Image, Text, View } from 'react-native'
+import IconButton from "./IconButton"
+
 const ProfileDetails = () => {
   return (
     <View className="flex justify-center items-center">
-      <Image 
-        source={require('../assets/images/icon.png')} 
+      <Image
+        source={require('../assets/images/icon.png')}
         className="max-h-36 max-w-36 rounded-full"
       />
       <Text className="text-2xl font-bold">Milton Velasquez</Text>
       <Text className="text-xl font-bold text-gray-400">Profesor</Text>
 
+      <IconButton icon="logo-instagram" pulsar={() => console.log("Instagram")} color="pink"></IconButton>
+      <IconButton icon="logo-whatsapp" pulsar={() => console.log("Whatsapp")} color="green"></IconButton>
+      <IconButton icon="logo-facebook" pulsar={() => console.log("Facebook")} color="blue"></IconButton>
+      <IconButton icon="logo-linkedin" pulsar={() => console.log("Linkedin")} color="lightblue"></IconButton>
 
-      
+
     </View>
   )
 }
